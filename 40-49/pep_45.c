3bc5ca8577eb01f246297b2a5aa871c15d845ef9
sayantan3 <sayantand975@gmail.com>
2021-05-20 20:39:32 +0530

    modification

--- 40-49/pep_45.c
+++ 40-49/pep_45.c
@@ -1,15 +1,22 @@
 #include<stdio.h>
 #include<math.h>
-
+#include<time.h>
 int is_triangle(unsigned long long n);
 int pentagonal(unsigned long long n);
 unsigned long long hexagonal(unsigned long long n);
 
 int main(void){
  	
+	double time_spent = 0;
+	clock_t begin = clock();
+
 	unsigned long long h, i;
 	for (i = 144; !(pentagonal(h = hexagonal(i)) && is_triangle(h)); i++);
 	printf("%llu \n", h);	
+
+	clock_t end = clock();
+	time_spent += (double) (end - begin) / CLOCKS_PER_SEC;
+	printf("\n %f in sec ", time_spent);
 
 	return 0;
 }
