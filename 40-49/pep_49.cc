8956daa0538fa3ae5f383202b8a6b1eb9922d57b
sayantan3 <sayantand975@gmail.com>
2021-06-09 18:42:48 +0530

    update

--- 40-49/pep_49.cc
+++ 40-49/pep_49.cc
@@ -16,35 +16,32 @@ struct func {
 };
 
 int main(){
+  bool seen[10000];
+  fill(seen, seen+10000, false);
+  for (int i = 1000; i < 10000; i++){
+    vector<int> a(4);
+    for (int j = 0, t = i; j < 4; j++, t /= 10){
+      a[3 - j] = t % 10;
+      }
+      vector<int> s;
+      do{
+        int n = accumulate(a.begin(), a.end(), 0, func<int>());
+        if (!seen[n] && is_prime(n)){
+          s.push_back(n);
+          seen[n];
+          }
+        } 
 
-	bool seen[10000];
-	fill(seen, seen+10000, false);
-	for (int i = 1000; i < 10000; i++){
-	       vector<int> a(4);
-       for (int j = 0, t = i; j < 4; j++, t /= 10){
-	a[3 - j] = t % 10;
-       }
-	vector<int> s;
- 	do{
-	int n = accumulate(a.begin(), a.end(), 0, func<int>());
-
-	if (!seen[n] && is_prime(n)){
-
-	s.push_back(n);
-	seen[n];
-	}
-	}
-	while (next_permutation(a.begin(), a.end()));
-
-	for (int j = 0; j < s.size(); j++){
-		for (int k = j+1; k < s.size(); k++){
-			for (int l = k+1; l < s.size(); l++){
-				if (s[j] != 1487 && s[l] - s[k] == s[k] - s[j]){
-						cout << s[j] << s[k] << s[l] << "\n";
-						}
-          }						
-        }				
-      }			
+  while (next_permutation(a.begin(), a.end()));
+  for (int j = 0; j < s.size(); j++){
+    for (int k = j+1; k < s.size(); k++){
+      for (int l = k+1; l < s.size(); l++){
+        if (s[j] != 1487 && s[l] - s[k] == s[k] - s[j]){
+          cout << s[j] << s[k] << s[l] << "\n";
+          }
+        }						
+      }				
+    }			
   }
   return 0;
 }
