9deb8267e14cae53bc3888175f272d1b4e4d9c5d
sayantan3 <sayantand975@gmail.com>
2021-07-02 18:46:38 +0530

    problem 63

---
+++ pep_63.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+def calculate():
+    ans = sum(1
+            for i in range(1, 10)
+            for j in range(1, 22)
+            if len(str(i**j)) == j)
+    return str(ans)
+
+if __name__ == "__main__":
+    print(calculate())
+
