a85f77ffc8fb956a307f9aa01d71d4128dd4e959
sayantan3 <sayantand975@gmail.com>
2021-07-03 23:41:13 +0530

    problem 55

---
+++ pep_55.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+def calculate():
+    ans = sum(1 for i in range(10000) if is_lychrel(i))
+    return str(ans)
+
+def is_lychrel(n):
+    for i in range(50):
+        n += int(str(n)[ : : -1])
+        if str(n) == str(n)[ : : -1]:
+            return False
+    return True
+
+if __name__ == "__main__":
+    print(calculate())
+
+
