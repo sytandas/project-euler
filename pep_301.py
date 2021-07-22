8a6472a77ce07bd6328913f924162a31169e1077
sayantan3 <sayantand975@gmail.com>
2021-07-22 20:27:21 +0530

    problem 301

---
+++ pep_301.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+def calculate():
+    a = 0
+    b = 1
+    for i in range(32):
+        a, b = b, a + b
+    return str(a)
+
+if __name__ == "__main__":
+    print(calculate())
