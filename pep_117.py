b3dceb79cc81f7ffefb7b71bcbd45de2befd1248
sayantan3 <sayantand975@gmail.com>
2021-07-26 23:07:10 +0530

    problem 117

---
+++ pep_117.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+def calculate():
+    LENGTH = 50
+    ways_c = [1] + [0] * LENGTH 
+    for n in range(1, len(ways_c)):
+        ways_c[n] += sum(ways_c[max(n - 4, 0) : n])
+    return str(ways_c[-1])
+
+if __name__ == "__main__":
+    print(calculate())
